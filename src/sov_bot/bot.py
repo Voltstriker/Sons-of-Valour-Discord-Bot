cc0504cb6e4bfd844f1384281076cfed322d4de8
Jayden Earles <mail@jaydenearles.com>
2025-11-04 21:12:09 +1100

    refactor: Move entry point into a dedicated src folder

---
+++ src/sov_bot/bot.py
@@ -0,0 +1,142 @@
+# Copyright 2025 Sons of Valour
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This file is part of the Sons of Valour Discord Bot project, designed to enhance the community experience for Sons of Valour members on Discord.
+
+The bot provides various features such as:
+    - moderation tools
+    - event management
+    - community engagement functionalities
+
+This project is maintained by the Sons of Valour community and is open for contributions. For more information, please refer to the project's
+documentation and contribution guidelines.
+
+For more information, visit the [Sons of Valour website](https://www.sonsofvalour.net) or join our [Discord server](https://discord.gg/G8amSzV).
+
+This file is licensed under the Apache License, Version 2.0. See the LICENSE file
+"""
+
+import os
+import logging
+import random
+import platform
+
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+intents = discord.Intents.default()
+
+
+class LoggingFormatter(logging.Formatter):
+    # Colors
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format = format.replace("(black)", self.black + self.bold)
+        format = format.replace("(reset)", self.reset)
+        format = format.replace("(levelcolor)", log_color)
+        format = format.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
+
+
+logger = logging.getLogger("discord_bot")
+logger.setLevel(logging.INFO)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+# File handler
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter("[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{")
+file_handler.setFormatter(file_handler_formatter)
+
+# Add the handlers
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+class DiscordBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(os.getenv("PREFIX", "!")),
+            intents=intents,
+            help_command=None,  # Disable the default help command
+        )
+
+        self.logger = logger
+        self.bot_prefix = os.getenv("PREFIX")
+        self.invite_link = os.getenv("INVITE_LINK")
+
+    @tasks.loop(minutes=1.0)
+    async def status_task(self) -> None:
+        """
+        Setup the game status task of the bot.
+        """
+        statuses = ["with you!", "with Krypton!", "with humans!"]
+        await self.change_presence(activity=discord.Game(random.choice(statuses)))
+
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        """
+        Before starting the status changing task, we make sure the bot is ready
+        """
+        await self.wait_until_ready()
+
+    async def setup_hook(self) -> None:
+        """
+        This will just be executed when the bot starts the first time.
+        """
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+        self.logger.info("-------------------")
+
+    async def on_message(self, message: discord.Message) -> None:
+        """
+        The code in this event is executed every time someone sends a message, with or without the prefix
+
+        :param message: The message that was sent.
+        """
+        if message.author == self.user or message.author.bot:
+            return
+        await self.process_commands(message)
+
+
+bot = DiscordBot()
+bot.run(os.getenv("TOKEN"))
